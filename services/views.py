e9993b1d25356a018374d96d471bb5f2b1ce5619
WhoCares <itc0r3@gmail.com>
2016-07-24 02:25:51 +0300

    Added endpoint responsible for setting language option in cookies.

--- services/views.py
+++ services/views.py
@@ -10,8 +10,13 @@ import re
 
 blueprint = Blueprint('services', __name__)
 
+
+
 @blueprint.route('/a')
 def autocomplete():
+    """
+    barebone autocomplete for mongo, probably should not be used on a large scale because of how taxing regex is
+    """
     query = request.args.get('q')
     rgx = re.compile(r'\b'+ re.escape(query), re.IGNORECASE)
     autocomplete_results = db.find({'$or':[
@@ -21,3 +26,18 @@ def autocomplete():
     autocomplete_results = [(r['title'], r['url_slug']) for r in autocomplete_results]
 
     return jsonify(autocomplete_results=autocomplete_results)
+
+
+@blueprint.route('/lang')
+def set_lang():
+    """
+    Endpoint responsible for setting language option in cookie.
+    After setting the cookie redirects to url supplied in parameters.
+    """
+    args = request.args
+    lang = args.get('lang')
+    redirect_url = args.get('url')
+
+    session['lang'] = lang
+    return redirect(redirect_url)
+
